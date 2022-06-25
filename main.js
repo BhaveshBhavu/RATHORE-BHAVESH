56063b8b19bd3ccf686cde5b6fe068f8138fd755
bhaveshabcd1234bhavu@gmail.com <bhaveshabcd1234bhavu@gmail.com>
2022-06-25 19:13:42 +0530

    omfg

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('Hello World!');
+
+
+function menu(){
+  var a = document.getElementById('menu');
+  
+  var b = document.getElementById('hedar');
+  
+  a.classList.toggle('menu_display_');
+  b.classList.toggle('menu_bg_c');
+  
+  
+  
+  
+}
+
